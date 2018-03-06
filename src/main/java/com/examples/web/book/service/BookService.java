35d0eff763e4bd9b98e3a593c6ce931924302439
Sandhya <sandhyaj224@gmail.com>
2018-03-06 12:16:25 -0500

    First Commit

---
+++ src/main/java/com/examples/web/book/service/BookService.java
@@ -0,0 +1,25 @@
+package com.examples.web.book.service;
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+import com.examples.web.book.Book;
+
+import java.util.List;
+
+public interface BookService {
+
+    Book save(Book book);
+
+    void delete(Book book);
+
+    Book findOne(String id);
+
+    Iterable<Book> findAll();
+
+    Page<Book> findByAuthor(String author, PageRequest pageRequest);
+
+    List<Book> findByTitle(String title);
+
+}
---
+++ src/main/java/com/examples/web/repository/BookRepository.java
@@ -0,0 +1,17 @@
+package com.examples.web.repository;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+import com.examples.web.book.Book;
+
+public interface BookRepository extends ElasticsearchRepository<Book, String> {
+
+    Page<Book> findByAuthor(String author, Pageable pageable);
+
+    List<Book> findByTitle(String title);
+
+}
